dae1141167f20cb0216c0d668ff97be22f9ebb71
Administrator <Administrator@501-S13>
2022-04-25 10:39:50 +0900

    0425 원,삼각형 넓이 구하기

--- 0425/src/TriangleExam.java
+++ 0425/src/TriangleExam.java
@@ -5,25 +5,34 @@ public class TriangleExam {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("밑변과 높이를 입력해주세요: ");
-		int line = sc.nextInt();
-		int high = sc.nextInt();
+		double line = sc.nextInt();
+		double high = sc.nextInt();
 		sc.close();
 		
 		Triangle t = new Triangle(line, high);
 		System.out.println("밑변이 "+line+"이고 높이가 "+high+"인 삼각형의 넓이는 "+t.T_size()+"입니다.");
+		t.T_setsize(2, 5);
+		System.out.println("밑변이 "+t.line+"이고 높이가 "+t.high+"인 삼각형의 넓이는 "+t.T_size()+"입니다.");
+		
 	}
 
 }
 
 
 class Triangle{
-	int line;
-	int high;
+	double line;
+	double high;
 	
-	public Triangle(int line,int high) {
+	public Triangle(double line,double high) {
 		this.line = line;
 		this.high = high;
 	}
+	
+	public void T_setsize(double a, double b){
+		line = a;
+		high = b;
+	}
+	
 	
 	public double T_size() {
 		return (line*high)/2;
